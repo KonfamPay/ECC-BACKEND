daf7f748cfa009ce5e67411d1a008d5008d7597c
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-09-03 20:00:42 +0100

    Extracted code from the index.js file and made it more modular

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log(`The app is now listening on port ${4000}`));
+require("express-async-errors");
+require("./passport-config");
+require("dotenv").config();
+require("./startup/config")();
+require("./startup/middleware")(app);
+require("./startup/routes")(app);
+require("./startup/db")();
+
+console.clear();
+const server = app.listen(port, () =>
+  console.log(`The app is now listening on port ${port}...`)
+);
