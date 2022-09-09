1fcfbed0ae3692b3efa1a9b6b9f602ae409eebac
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-09-09 22:39:31 +0100

    Added the api route and the default 404 error route

--- startup/routes.js
+++ startup/routes.js
@@ -3,11 +3,16 @@ const users = require("../routes/users");
 const forgotPassword = require("../routes/forgot-password");
 const resetPassword = require("../routes/reset-password");
 const error = require("../middleware/error");
+const image = require("../routes/image");
 
 module.exports = (app) => {
   app.use("/api/users", users);
   app.use("/api/auth", auth);
   app.use("/api/forgot-password", forgotPassword);
   app.use("/api/reset-password", resetPassword);
+  app.use("/api/image", image);
+  app.get("*", (req, res) => {
+    res.sendStatus(404);
+  });
   app.use(error);
 };
