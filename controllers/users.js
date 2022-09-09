15124879c336122efdaa8adf676252efe5e70138
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-09-09 22:36:56 +0100

    Added the controller for verifying users

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,10 @@
 const bcrypt = require("bcrypt");
-const { User, validateUser: validate } = require("../models/user");
+const {
+  User,
+  validateUser: validate,
+  validateVerifyInputs,
+} = require("../models/user");
+const mongoose = require("mongoose");
 
 const createNewUser = async (req, res) => {
   const { error } = validate(req.body);
@@ -20,4 +25,53 @@ const createNewUser = async (req, res) => {
   const token = user.generateAuthToken();
   res.status(201).json({ token });
 };
-module.exports = { createNewUser };
+
+const verifyUser = async (req, res) => {
+  const { id } = req.params;
+  if (!id)
+    return res
+      .status(400)
+      .json({ message: "The id is required in the body of the request!" });
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(400).json({ message: "This is not a valid mongoose id" });
+
+  const user = await User.findById(id);
+
+  if (!user)
+    return res.status(404).send({ message: "This user does not exist" });
+
+  if (user.verified)
+    return res
+      .status(400)
+      .send({ message: "This user has already been verified!" });
+
+  const { dob, phoneNumber, address, lga, state, photoId } = req.body;
+
+  const { error } = validateVerifyInputs({
+    dob,
+    phoneNumber,
+    address,
+    lga,
+    state,
+    photoIdUrl: photoId.url,
+  });
+
+  if (error) return res.status(400).send({ message: error.details[0].message });
+
+  user.dob = dob;
+  user.phoneNumber = phoneNumber;
+  user.address = address;
+  user.lga = lga;
+  user.state = state;
+  user.photoId = photoId;
+
+  user.verified = true;
+  await user.save();
+
+  const newToken = await user.generateAuthToken();
+
+  res.status(200).json({ token: newToken });
+};
+
+module.exports = { createNewUser, verifyUser };
