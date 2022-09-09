ac349f9b59d04b0e607e069a6ae7f62fa2eedf11
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-09-09 22:39:03 +0100

    Added the morgan package for logging

--- startup/middleware.js
+++ startup/middleware.js
@@ -2,9 +2,11 @@ const passport = require("passport");
 const express = require("express");
 const cors = require("cors");
 const session = require("express-session");
+const logger = require("morgan");
 
 module.exports = (app) => {
   app.use(express.json());
+  app.use(logger("dev"));
   app.use(express.urlencoded({ extended: false }));
   app.use(session({ secret: process.env.SESSION_SECRET }));
   app.use(passport.initialize());
