199729030c1677fc43235967b0fb66de6c7e8be4
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-09-27 15:43:04 +0100

    Created the controller for getting notification and the notification service

---
+++ controllers/notification.js
@@ -0,0 +1,34 @@
+const { response } = require("express");
+const mongoose = require("mongoose");
+const {
+  Notification,
+  validateNotification,
+} = require("../models/Notification");
+const { User } = require("../models/user");
+
+const getAllNotifications = async (req, res) => {
+  const { userId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(userId))
+    return res.status(404).json({ message: "This id is not valid!" });
+
+  const user = await User.findOne({ _id: userId });
+  if (!user)
+    return res
+      .status(404)
+      .json({ message: "This user does not exist in the database!" });
+
+  const notifications = await Notification.find({ userId });
+  return res.status(200).json({ notifications });
+};
+
+class NotificationService {
+  create = async (notificationPayload) => {
+    const { error } = validateNotification(notificationPayload);
+    if (error) throw new Error(error.details[0].message);
+    const notification = new Notification(notificationPayload);
+    return await notification.save();
+  };
+}
+
+module.exports = { getAllNotifications, NotificationService };
