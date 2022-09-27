65624aa3666ef31a99f1da3152049f8dca3d5b6e
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-09-27 11:42:10 +0100

    Added timestamps to the complaint model

--- models/complaint.js
+++ models/complaint.js
@@ -1,120 +1,123 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
-const complaintSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
+const complaintSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
 
-  title: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 100,
-  },
-  complaintLocation: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 100,
-  },
-  brandName: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 50,
-  },
-  brandContact: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 75,
-  },
-  productCategory: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 50,
-  },
-  brandBankAccountNumber: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 30,
-  },
-  brandBankAccountName: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 50,
-  },
-  brandBank: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 50,
-  },
-  brandSocialMediaHandle: {
-    type: String,
-    required: false,
-    minlength: 3,
-    maxlength: 50,
-  },
-  transactionReceipt: {
-    url: {
+    title: {
       type: String,
       required: true,
-      maxlength: 255,
-      default: "",
+      minlength: 5,
+      maxlength: 100,
     },
-    cloudinaryId: {
+    complaintLocation: {
       type: String,
       required: true,
-      maxlength: 255,
-      default: "",
+      minlength: 5,
+      maxlength: 100,
     },
-  },
-  additionalDocuments: {
-    type: [
-      {
-        url: {
-          type: String,
-          required: false,
-          maxlength: 255,
-          default: "",
-        },
-        cloudinaryId: {
-          type: String,
-          required: false,
-          maxlength: 255,
-          default: "",
-        },
+    brandName: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    brandContact: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 75,
+    },
+    productCategory: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    brandBankAccountNumber: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    brandBankAccountName: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    brandBank: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+    },
+    brandSocialMediaHandle: {
+      type: String,
+      required: false,
+      minlength: 3,
+      maxlength: 50,
+    },
+    transactionReceipt: {
+      url: {
+        type: String,
+        required: true,
+        maxlength: 255,
+        default: "",
       },
-    ],
-    default: [],
+      cloudinaryId: {
+        type: String,
+        required: true,
+        maxlength: 255,
+        default: "",
+      },
+    },
+    additionalDocuments: {
+      type: [
+        {
+          url: {
+            type: String,
+            required: false,
+            maxlength: 255,
+            default: "",
+          },
+          cloudinaryId: {
+            type: String,
+            required: false,
+            maxlength: 255,
+            default: "",
+          },
+        },
+      ],
+      default: [],
+    },
+    complaintAmount: {
+      type: Number,
+      required: true,
+      min: [0, "The complaint amount has to be greater than 0"],
+    },
+    status: {
+      type: String,
+      enum: ["resolved", "open", "pending", "closed"],
+      default: "pending",
+    },
+    details: {
+      type: String,
+      max: [2500, "This field has a maximum of 2500 characters"],
+    },
+    resolution: {
+      type: String,
+      enum: ["refund", "compensation", "apology", "replacement"],
+      required: true,
+      maxlength: 60,
+    },
   },
-  complaintAmount: {
-    type: Number,
-    required: true,
-    min: [0, "The complaint amount has to be greater than 0"],
-  },
-  status: {
-    type: String,
-    enum: ["resolved", "open", "pending", "closed"],
-    default: "pending",
-  },
-  details: {
-    type: String,
-    max: [2500, "This field has a maximum of 2500 characters"],
-  },
-  resolution: {
-    type: String,
-    enum: ["refund", "compensation", "apology", "replacement"],
-    required: true,
-    maxlength: 60,
-  },
-});
+  { timestamps: true }
+);
 
 const validateComplaint = (payload) => {
   const schema = Joi.object({
