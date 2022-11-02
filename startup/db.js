3c65e016906c279ed5c020e78af77d25846d2a1b
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-11-02 08:45:19 +0100

    Chore: Updated mongob connection string

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-	mongoose.connect(process.env.DB_CONN_STRING, () => {
+	mongoose.connect("mongodb+srv://itzadetunji:adetunjimay29@ecc.nnqlkbz.mongodb.net/ecc?retryWrites=true&w=majority", () => {
 		console.log("Connected to the Database...");
 	});
 };
