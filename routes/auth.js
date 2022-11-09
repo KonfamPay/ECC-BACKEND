7973aa14843ca989d6f548d0aef04d1c5b7225f7
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-11-09 13:46:44 +0100

    Chore: Removed unsued files

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const {
-  authenticateUser,
-  signInWithGoogle,
-  googleCallback,
-  googleSignInSuccessful,
-  googleSignInFailed,
+	authenticateUser,
+	signInWithGoogle,
+	googleCallback,
+	googleSignInSuccessful,
+	googleSignInFailed,
 } = require("../controllers/auth");
 const authRouter = express.Router();
 
@@ -13,6 +13,6 @@ authRouter.post("/google", signInWithGoogle);
 authRouter.get("/google/callback", googleCallback, googleSignInSuccessful);
 authRouter.get("/failed", googleSignInFailed);
 authRouter.get("/google/start", (req, res) => {
-  res.redirect("/api/auth/google");
+	res.redirect("/api/auth/google");
 });
 module.exports = authRouter;
--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
+	// mongoose.connect("mongodb://127.0.0.1:27017/ECC", () => {
+	// 	console.log("Connected to the Database...");
+	// });
 	mongoose.connect("mongodb+srv://itzadetunji:adetunjimay29@ecc.nnqlkbz.mongodb.net/ecc?retryWrites=true&w=majority", () => {
 		console.log("Connected to the Database...");
 	});
