14a1119285e8416190c449339c1b2f672f5554e0
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-15 12:52:55 +0100

    Chore: Added middleware to verify lead admin before creating a new admin

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,6 @@ const adminRouter = express.Router();
 const { createAdmin, adminLogin } = require("../controllers/adminController");
 const { leadAdmin } = require("../middleware/admin");
 
-adminRouter.post("/", createAdmin);
+adminRouter.post("/", leadAdmin, createAdmin);
 adminRouter.post("/login", adminLogin);
 module.exports = adminRouter;
