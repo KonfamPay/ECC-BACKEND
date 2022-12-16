466de8b02190bcff78d6680b5374a11f95249167
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-17 00:23:56 +0100

    Feat: Removed create activity and updated activity controller

--- routes/activity.js
+++ routes/activity.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const activityRouter = express.Router();
 const {
-	createNewActivitdy,
 	getAllActivity,
 } = require("../controllers/activityController");
 const { admin } = require("../middleware/admin");
 
-activityRouter.post("/", admin, createNewActivitdy);
 activityRouter.get("/", admin, getAllActivity);
 
 module.exports = activityRouter;
