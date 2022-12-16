6894f66559afa6e3f907dcfca632468ead98553f
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-17 00:28:29 +0100

    Feat: Adding middlewares for protected routes

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const usersRouter = express.Router();
 const {
-  createNewUser,
-  verifyAccount,
-  verifyUserEmail,
-  resendVerifyEmailCode,
-  deleteUser
+	createNewUser,
+	verifyAccount,
+	verifyUserEmail,
+	resendVerifyEmailCode,
+	deleteUser,
 } = require("../controllers/usersController");
+const { admin } = require("../middleware/admin");
 
 usersRouter.post("/", createNewUser);
 usersRouter.post("/verify/:id", verifyAccount);
 usersRouter.post("/verify_email/:id", verifyUserEmail);
 usersRouter.post("/verify_email/:id/resend_code", resendVerifyEmailCode);
-usersRouter.post("/delete/:id", deleteUser);
+usersRouter.post("/delete/:id", admin, deleteUser);
 
 module.exports = usersRouter;
