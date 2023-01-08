3fe15ccc619b8e1c4ef592b34bb576c344d8daeb
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2023-01-08 07:18:28 +0100

    Feat: Created cookie extractor and added cookie for authentication

--- startup/middleware.js
+++ startup/middleware.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const session = require("express-session");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
 
 const trimmer = (req, res, next) => {
 	if (req.method === "POST") {
@@ -12,17 +15,26 @@ const trimmer = (req, res, next) => {
 		}
 	}
 	next();
-}
+};
 
 module.exports = (app) => {
+	app.use(cookieParser());
 	app.use(express.json());
-	app.use(logger("dev"));
 	app.use(express.urlencoded({ extended: false }));
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+	app.use(logger("dev"));
 	app.use(session({ secret: process.env.SESSION_SECRET }));
 	app.use(passport.initialize());
 	app.use(express.static("public"));
 	app.set("view engine", "ejs");
 	app.use(cors());
-	app.use(bodyParser.json());
+	app.use(helmet());
 	app.use(trimmer);
+	app.use(
+		fileUpload({
+			useTempFiles: true,
+			limits: { fileSize: 50 * 1024 * 1024 },
+		})
+	);
 };
