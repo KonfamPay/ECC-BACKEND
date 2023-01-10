994ef5a79752b7fe4b8cb527d390fba9d0b0adff
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2023-01-10 17:21:44 +0100

    Chore: Testing something else

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,9 @@
 const express = require("express");
 const adminRouter = express.Router();
 const {
-	createAdmin,
-	getAdminDetails,
-	adminLogin,
-	veifyAdminLogin,
-	deleteAdmin,
 	resendVerifyEmailCode,
 } = require("../controllers/adminController");
-const { admin, leadAdmin } = require("../middleware/admin");
+// const { admin, leadAdmin } = require("../middleware/admin");
 
-adminRouter.post("/", leadAdmin, createAdmin);
-adminRouter.get("/dashboard", admin, getAdminDetails);
-adminRouter.post("/login", adminLogin);
-adminRouter.post("/login/verify/:adminId/:otp", veifyAdminLogin);
-adminRouter.delete("/:id", leadAdmin, deleteAdmin);
 adminRouter.post("/verify_email/:id/resend_code", resendVerifyEmailCode);
 module.exports = adminRouter;
