4910eca4039abc5de22d64dc07bc68e885213d60
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2023-01-10 17:57:23 +0100

    Chore: Removed updated and started all over

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const adminRouter = express.Router();
 const {
+	createAdmin,
+	getAdminDetails,
+	adminLogin,
+	veifyAdminLogin,
+	deleteAdmin,
 	resendVerifyEmailCode,
-} = require("../controllers/adcon");
-// const { admin, leadAdmin } = require("../middleware/admin");
+} = require("../controllers/adminController");
+const { admin, leadAdmin } = require("../middleware/admin");
 
+adminRouter.post("/", leadAdmin, createAdmin);
+adminRouter.get("/dashboard", admin, getAdminDetails);
+adminRouter.post("/login", adminLogin);
+adminRouter.post("/login/verify/:adminId/:otp", veifyAdminLogin);
+adminRouter.delete("/:id", leadAdmin, deleteAdmin);
 adminRouter.post("/verify_email/:id/resend_code", resendVerifyEmailCode);
 module.exports = adminRouter;
